d6325a31fd2da1baee4fa0d85f21eca5b20d3a3f
Cloe Kouadjo <armelfli50@gmail.com>
2019-08-18 14:20:17 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -3,6 +3,10 @@ class CashRegister
   def initialize(employe_discount = nil)
     @total = 0 
     @discount = employe_discount
+  end 
+  
+  def add_item(title, price)
+    @total += price
   end
   
 end
