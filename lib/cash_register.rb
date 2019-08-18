f87e8b4e1e4a54036f17493eeb71459410f0c1d2
Cloe Kouadjo <armelfli50@gmail.com>
2019-08-18 14:51:45 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -44,7 +44,7 @@ class CashRegister
   def void_last_transaction  
     @last_item_price[-1] == nil ? @total = 0.0 :  @total -= @last_item_price[-1]  
    
-    @last_item_price.pop 
+  
     
   end
   
